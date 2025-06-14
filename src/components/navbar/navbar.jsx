946027214883551f48a1a42a69b3d16b5acd3698
NESuchi <nicoeberhardt96@gmail.com>
2025-06-14 03:15:50 +0200

    Navbar Design

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,32 @@
+import styles from "./navbar.module.css"
 import logo from '@/assets/logo.png';
 import { Link } from 'react-router-dom'
 import { IoLogIn } from "react-icons/io5";
 
 const Navbar = () => {
     return (
-        <div>
-            <img
-                src={logo}
-                height={50}
-                width={50}
-                alt="logo"
-            />
-            <div>
-                <Link to="/">Home</Link> | {" "}
-                <Link to="/food-tracker">Food Tracker</Link> | {" "}
-                <Link to="/exercises">Exercises</Link> | {" "}
-                <Link to="/profile">Profile</Link> | {" "}
-                <Link to="/kalender">Kalender</Link>
+        <div className={styles.Wrapper}>
+            <div className={styles.Navbar}>
+                <div className={styles.Img}>
+                    <img
+                        className={styles.Logo}
+                        src={logo}
+                        height={117}
+                        width={117}
+                        alt="logo"
+                    />
+                </div>
+                    <div className={styles.Navigation}>
+                        <Link to="/">Home</Link> | {" "}
+                        <Link to="/food-tracker">Food Tracker</Link> | {" "}
+                        <Link to="/exercises">Exercises</Link> | {" "}
+                        <Link to="/profile">Profile</Link> | {" "}
+                        <Link to="/kalender">Kalender</Link>
+                    </div>
+                    <Link to="/login" className={styles.Login}>
+                        <IoLogIn size={50} />
+                    </Link>                    
             </div>
-            <Link to="/login">
-                <IoLogIn />
-            </Link>
         </div>
     )
 }
